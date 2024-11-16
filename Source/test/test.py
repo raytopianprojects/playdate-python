0616e46c51c2a1b2392a314f840002ac93f42dd4
Raytopian Projects <54505044+raytopianprojects@users.noreply.github.com>
2024-11-15 20:50:13 -0700

    Add comment for how importing works.

--- Source/test/test.py
+++ Source/test/test.py
@@ -1,3 +1,5 @@
 import pd
+# Because this file is called test you can expose variables from it using its name, but when importing it you'll need to
+# use its full path: test_test
 test.x = 10
 print(test.x, "HI")
